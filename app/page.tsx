bd8b26db13d1fcb286499b3c988209559f533a25
DB Gautam <gautamdb28@gmail.com>
2023-08-17 23:11:29 +0545

    feat: components installed and setup for the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import EventCalendar from "@/components/customUi/eventCalendar"
+import { DataTable } from "@/components/customUi/dataTable"
+import { ToggleTheme } from "@/components/customUi/toggleTheme"
 import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Shade cn ui
       <Button>Click me</Button>
-      <EventCalendar />
+     <DataTable />
+  <ToggleTheme />
     </main>
   )
 }
