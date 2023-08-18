33f3c1102b2f1d509d55c1c666c3e0bbae88d899
DB Gautam <gautamdb28@gmail.com>
2023-08-18 12:49:45 +0545

    integrate:ui components setup and installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { DataTable } from "@/components/customUi/dataTable"
+"use client"
+import { DateRangePicker } from "@/components/customUi/dateRangePicker"
 import { ToggleTheme } from "@/components/customUi/toggleTheme"
 import { Button } from "@/components/ui/button"
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Shade cn ui
       <Button>Click me</Button>
-     <DataTable />
+   <DateRangePicker />
   <ToggleTheme />
     </main>
   )
